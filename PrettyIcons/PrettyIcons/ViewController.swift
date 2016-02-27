22615f061eff77a4117b7394c8b450a0c9d2b86c
Alexander Pronin <pronin.alx@gmail.com>
2016-02-27 11:54:34 +0200

    Final commit

--- PrettyIcons/PrettyIcons/ViewController.swift
+++ PrettyIcons/PrettyIcons/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
+    
+    // MARK: - Vars
+    var icons = [Icon]()
+    
+    // MARK: - UIViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let iconSets = IconSet.iconSets()
+        let iconSet = iconSets[0]
+        icons = iconSet.icons
+        automaticallyAdjustsScrollViewInsets = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return icons.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("IconCell", forIndexPath: indexPath)
+        let icon = icons[indexPath.row]
+        cell.textLabel?.text = icon.title
+        cell.detailTextLabel?.text = icon.subtitle
+        if let iconImage = icon.image {
+            cell.imageView?.image = iconImage
+        }
+        return cell
+    }
+}
